a069d6f3a3d5a55c143289e9e203208a04cd5a40
Prem,Witthawas <83864634+premwitthawas@users.noreply.github.com>
2024-12-13 17:26:47 +0700

    add pg user mock

--- user-service/postgres-init/init.sql
+++ user-service/postgres-init/init.sql
@@ -7,3 +7,8 @@ CREATE TABLE IF NOT EXISTS public."Users"
     "Gender" character varying(15) COLLATE pg_catalog."default",
     CONSTRAINT "Users_pkey" PRIMARY KEY ("UserID")
 );
+
+INSERT INTO public."Users" ("UserID", "Email", "PersonName", "Gender", "Password")
+VALUES 
+('c32f8b42-60e6-4c02-90a7-9143ab37189f', 'test1@example.com', 'John Doe', 'Male', 'password1'),
+('8ff22c7d-18c7-4ef0-a0ac-988ecb2ac7f5', 'test2@example.com', 'Jane Smith', 'Female', 'password2');
